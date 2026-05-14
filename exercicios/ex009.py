a8141da4667b93bf0c5bfd1e9434658ae9d3065c
v1souto <souto2096@gmail.com>
2026-05-13 22:14:21 -0300

    PythonTeste

--- exercicios/ex009.py
+++ exercicios/ex009.py
@@ -1,4 +1,5 @@
 num = int(input('Digite um número para ver sua tabuada:'))
+print('-' *12)
 print('{} x {:2} = {}'.format(num, 1, num*1))
 print('{} x {:2} = {}'.format(num, 2, num*2))
 print('{} x {:2} = {}'.format(num, 3, num*3))
@@ -9,5 +10,6 @@ print('{} x {:2} = {}'.format(num, 7, num*7))
 print('{} x {:2} = {}'.format(num, 8, num*8))
 print('{} x {:2} = {}'.format(num, 9, num*9))
 print('{} x {:2} = {}'.format(num, 10, num*10))
+print('-'*12)
 
 
