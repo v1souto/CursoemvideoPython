b4100152b9fec0e6dd07387843f7cfa6f90b52e4
v1souto <souto2096@gmail.com>
2026-05-15 08:43:20 -0300

    PythonTeste

---
+++ exercicios/ex017.py
@@ -0,0 +1,5 @@
+from math import hypot
+co = float(input('Comprimnto do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
